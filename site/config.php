32a15e3a9b47849bfccdb4238c18fa4168df603a
Wishrito <contact.rayantremion@gmail.com>
2024-11-15 15:04:56 +0100

    created base website class

---
+++ site/config.php
@@ -0,0 +1,81 @@
+<?php
+
+class SiteConfig
+{
+    private $attributes = [
+        'site_name' => '',
+        'base_url' => '',
+        'host' => '',
+        'db' => '',
+        'user' => '',
+        'pass' => '',
+        // Add more attributes here as needed
+    ];
+
+    // Constructor to initialize attributes
+    public function __construct($site_name = '', $host = 'localhost', $db = '', $user = 'root', $pass = '')
+    {
+        $this->setAttribute('site_name', $site_name);
+        $this->setAttribute('host', $host);
+        $this->setAttribute('db', $db);
+        $this->setAttribute('user', $user);
+        $this->setAttribute('pass', $pass);
+    }
+
+    // function to get the base path
+    function base_url(): string
+    {
+        // Détecter si HTTPS est activé
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+
+        // Récupérer le nom de domaine et le port
+        $domainName = $_SERVER['HTTP_HOST'];
+
+        // Chemin de base du script (si l'application est dans un sous-dossier)
+        $basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+
+        // Construire et retourner l'URL complète
+        return "$protocol$domainName$basePath/";
+    }
+
+    // Generic setter for any attribute
+    public function setAttribute(string $key, string $value): void
+    {
+        $this->attributes[$key] = $value;
+    }
+
+    // Generic getter for any attribute
+    public function getAttribute(string $key): string|null
+    {
+        return isset($this->attributes[$key]) ? $this->attributes[$key] : null;
+    }
+
+    // Getter for all attributes
+    public function getAllAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    public function setSiteName(string $site_name): void
+    {
+        $this->attributes['site_name'] = $site_name;
+    }
+
+    public function getSiteName(): string
+    {
+        return $this->attributes['site_name'];
+    }
+}
+
+$site = new SiteConfig("CRM Hopital");
+$site->setAttribute('db', 'crm_hopital');
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=" . $site->getAttribute('host') . ";dbname=" . $site->getAttribute('db') . ";charset=utf8", $site->getAttribute('user'), $site->getAttribute('pass'));
+    // Définir le mode d'erreur PDO sur Exception
+
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
---
+++ site/index.php
@@ -0,0 +1,15 @@
+<?php include "./config.php" ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $site->getAttribute('site_name') ?> - Accueil</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
