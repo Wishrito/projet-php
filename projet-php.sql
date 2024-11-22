e761c80f18bfd1f47fdf7cb45dc30c2c9ddf91b5
Wishrito <contact.rayantremion@gmail.com>
2024-11-22 11:39:14 +0100

    Update projet-php.sql

--- projet-php.sql
+++ projet-php.sql
@@ -16,106 +16,112 @@
 
 
 -- Listage de la structure de la base pour crm_hopital
-DROP DATABASE IF EXISTS `crm_hopital`;
 CREATE DATABASE IF NOT EXISTS `crm_hopital` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `crm_hopital`;
 
 -- Listage de la structure de table crm_hopital. consultation
-DROP TABLE IF EXISTS `consultation`;
 CREATE TABLE IF NOT EXISTS `consultation` (
-  `date` date NOT NULL,
+  `ID` int NOT NULL,
+  `debrief` text NOT NULL,
   `patient_id` int NOT NULL,
-  `employee_id` int NOT NULL,
-  `debrief` text,
-  PRIMARY KEY (`date`,`patient_id`,`employee_id`)
+  `medical_staff_id` int NOT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table crm_hopital.consultation : ~0 rows (environ)
 DELETE FROM `consultation`;
 
--- Listage de la structure de table crm_hopital. employee
-DROP TABLE IF EXISTS `employee`;
-CREATE TABLE IF NOT EXISTS `employee` (
-  `ID` int NOT NULL,
-  `service` int NOT NULL,
-  `job` int NOT NULL,
-  `hiring_date` date NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `FK_service` (`service`),
-  KEY `FK_job` (`job`),
-  CONSTRAINT `FK_job` FOREIGN KEY (`job`) REFERENCES `job` (`ID`),
-  CONSTRAINT `FK_service` FOREIGN KEY (`service`) REFERENCES `service` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='héritage de la table users, destiné aux employés';
-
--- Listage des données de la table crm_hopital.employee : ~0 rows (environ)
-DELETE FROM `employee`;
-
 -- Listage de la structure de table crm_hopital. job
-DROP TABLE IF EXISTS `job`;
 CREATE TABLE IF NOT EXISTS `job` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `libelle` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `libelle` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table crm_hopital.job : ~0 rows (environ)
 DELETE FROM `job`;
+INSERT INTO `job` (`ID`, `libelle`) VALUES
+	(1, 'Medecin'),
+	(2, 'Interne');
+
+-- Listage de la structure de table crm_hopital. medical_record
+CREATE TABLE IF NOT EXISTS `medical_record` (
+  `id` int NOT NULL,
+  `record_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `diagnosis` int DEFAULT NULL,
+  `doctor_id` int DEFAULT NULL,
+  `notes` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table crm_hopital.medical_record : ~0 rows (environ)
+DELETE FROM `medical_record`;
+
+-- Listage de la structure de table crm_hopital. medical_staff
+CREATE TABLE IF NOT EXISTS `medical_staff` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `first_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `last_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `birth_date` date NOT NULL,
+  `leaving_date` timestamp NULL DEFAULT NULL,
+  `hiring_date` date DEFAULT NULL,
+  `service` int NOT NULL,
+  `job` int NOT NULL,
+  PRIMARY KEY (`ID`) USING BTREE,
+  KEY `fk_staff_service` (`service`),
+  KEY `fk_staff_job` (`job`),
+  CONSTRAINT `fk_staff_job` FOREIGN KEY (`job`) REFERENCES `job` (`ID`),
+  CONSTRAINT `fk_staff_service` FOREIGN KEY (`service`) REFERENCES `service` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table crm_hopital.medical_staff : ~0 rows (environ)
+DELETE FROM `medical_staff`;
 
 -- Listage de la structure de table crm_hopital. message
-DROP TABLE IF EXISTS `message`;
 CREATE TABLE IF NOT EXISTS `message` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `content` text,
-  `file` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `date` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  CONSTRAINT `FK_message_users` FOREIGN KEY (`ID`) REFERENCES `users` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='stocke les infos sur les messages envoyés, comprenant la timestamp de chaque';
+  `ID` int NOT NULL,
+  `content` text NOT NULL,
+  `file` varchar(200) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sender` int NOT NULL,
+  `receiver` int NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table crm_hopital.message : ~0 rows (environ)
 DELETE FROM `message`;
 
 -- Listage de la structure de table crm_hopital. patient
-DROP TABLE IF EXISTS `patient`;
 CREATE TABLE IF NOT EXISTS `patient` (
-  `ID` int NOT NULL,
-  `floor_lvl` int DEFAULT NULL,
-  `admission_date` date DEFAULT NULL,
-  `background` text,
-  PRIMARY KEY (`ID`),
-  CONSTRAINT `FK_heritage` FOREIGN KEY (`ID`) REFERENCES `users` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='héritage de la table users, destiné aux patients';
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `first_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `last_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `birth_date` date NOT NULL,
+  `leaving_date` timestamp NULL DEFAULT NULL,
+  `floor_lvl` int DEFAULT '0',
+  `admission_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table crm_hopital.patient : ~0 rows (environ)
 DELETE FROM `patient`;
 
 -- Listage de la structure de table crm_hopital. service
-DROP TABLE IF EXISTS `service`;
 CREATE TABLE IF NOT EXISTS `service` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `libelle` text NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `libelle` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table crm_hopital.service : ~0 rows (environ)
 DELETE FROM `service`;
-
--- Listage de la structure de table crm_hopital. users
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `login` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `first_name` int DEFAULT NULL,
-  `last_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `birth_date` date DEFAULT NULL,
-  `leaving_date` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='contient les informations sur les utilisateurs';
-
--- Listage des données de la table crm_hopital.users : ~0 rows (environ)
-DELETE FROM `users`;
+INSERT INTO `service` (`ID`, `libelle`) VALUES
+	(1, 'Urgences'),
+	(2, 'Cardiologie');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
